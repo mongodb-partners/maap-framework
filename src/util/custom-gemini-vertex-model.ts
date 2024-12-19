e7c211a2a1e5f3bf68a53c7af38685a6a06cf82f
LuisV1296 <luis.valverde@pureinsights.com>
2024-12-19 10:21:11 -0600

    Fixes in custom-gemini-vertex-model

--- src/util/custom-gemini-vertex-model.ts
+++ src/util/custom-gemini-vertex-model.ts
@@ -1,6 +1,5 @@
-import type {
-    ToolCallLLMMessageOptions,
-} from "@llamaindex/core/llms";
+import type { ToolCallLLMMessageOptions } from '@llamaindex/core/llms';
+
 import {
     GEMINI_MODEL,
     type GeminiChatNonStreamResponse,
@@ -11,10 +10,7 @@ import {
     type GeminiModelInfo,
     type IGeminiSession,
 } from 'llamaindex/llm/gemini/types';
-import {
-    GeminiHelper,
-    getChatContext,
-} from 'llamaindex/llm/gemini/utils';
+import { GeminiHelper, getChatContext, mapBaseToolToGeminiFunctionDeclaration } from 'llamaindex/llm/gemini/utils';
 import { Gemini } from 'llamaindex';
 import { Tokenizers } from '@llamaindex/env/tokenizers';
 import { Content, Part, SafetySetting } from '@google-cloud/vertexai';
@@ -56,11 +52,11 @@ export type GeminiConfig = Partial<typeof DEFAULT_GEMINI_PARAMS> & {
 };
 
 export class CustomGemini extends Gemini {
-    private safety_settings
+    private readonly safety_settings: SafetySetting[];
 
     constructor(init?: GeminiConfig, safety_settings?: SafetySetting[]) {
         super(init);
-        this.safety_settings = safety_settings
+        this.safety_settings = safety_settings;
     }
 
     get supportToolCall(): boolean {
@@ -75,61 +71,76 @@ export class CustomGemini extends Gemini {
             maxTokens: this.maxTokens,
             contextWindow: GEMINI_MODEL_INFO_MAP[this.model].contextWindow,
             tokenizer: undefined,
-            safety_settings: this.safety_settings
+            safety_settings: this.safety_settings,
         };
     }
 
-    protected async nonStreamChat(
-        params: GeminiChatParamsNonStreaming,
-    ): Promise<GeminiChatNonStreamResponse> {
+    protected async nonStreamChat(params: GeminiChatParamsNonStreaming): Promise<GeminiChatNonStreamResponse> {
         const context = getChatContext(params);
         const client = this.session.getGenerativeModel(this.metadata);
-        const chat = client.startChat({
-                history: context.history as Content[],
-                safetySettings: this.safety_settings,
-                generationConfig: {
-                    maxOutputTokens: this.maxTokens,
-                    temperature: this.temperature,
-                    topP: this.topP
-                }
-            }
-        )
+        const chat = client.startChat(
+            params.tools
+                ? {
+                      history: context.history as Content[],
+                      safetySettings: this.safety_settings,
+                      tools: [
+                          {
+                              functionDeclarations: params.tools.map(mapBaseToolToGeminiFunctionDeclaration),
+                          },
+                      ],
+                  }
+                : {
+                      history: context.history as Content[],
+                      safetySettings: this.safety_settings,
+                  },
+        );
         const { response } = await chat.sendMessage(context.message as Part[]);
         const topCandidate = response.candidates![0]!;
 
         const tools = this.session.getToolsFromResponse(response);
-        const options: ToolCallLLMMessageOptions = tools?.length
-            ? { toolCall: tools }
-            : {};
+        const options: ToolCallLLMMessageOptions = tools?.length ? { toolCall: tools } : {};
 
         return {
             raw: response,
             message: {
                 content: this.session.getResponseText(response),
-                role: GeminiHelper.ROLES_FROM_GEMINI[
-                    topCandidate.content.role as GeminiMessageRole
-                    ],
+                role: GeminiHelper.ROLES_FROM_GEMINI[topCandidate.content.role as GeminiMessageRole],
                 options,
             },
         };
     }
 
-    override async *streamChat(
-        params: GeminiChatParamsStreaming,
-    ): GeminiChatStreamResponse {
+    protected async *streamChat(params: GeminiChatParamsStreaming): GeminiChatStreamResponse {
         const context = getChatContext(params);
         const client = this.session.getGenerativeModel(this.metadata);
-        const chat = client.startChat({
-                history: context.history as Content[],
-                safetySettings: this.safety_settings,
-                generationConfig: {
-                    maxOutputTokens: this.maxTokens,
-                    temperature: this.temperature,
-                    topP: this.topP
-                }
-            }
-        )
+        const chat = client.startChat(
+            params.tools
+                ? {
+                      history: context.history as Content[],
+                      safetySettings: this.safety_settings,
+                      tools: [
+                          {
+                              functionDeclarations: params.tools.map(mapBaseToolToGeminiFunctionDeclaration),
+                          },
+                      ],
+                  }
+                : {
+                      history: context.history as Content[],
+                      safetySettings: this.safety_settings,
+                  },
+        );
         const result = await chat.sendMessageStream(context.message as Part[]);
         yield* this.session.getChatStream(result);
+    }
+
+    chat(params: GeminiChatParamsStreaming): Promise<GeminiChatStreamResponse>;
+    chat(
+        params: GeminiChatParamsNonStreaming,
+    ): Promise<GeminiChatNonStreamResponse>;
+    async chat(
+        params: GeminiChatParamsStreaming | GeminiChatParamsNonStreaming,
+    ): Promise<GeminiChatStreamResponse | GeminiChatNonStreamResponse> {
+        if (params.stream) return this.streamChat(params);
+        return this.nonStreamChat(params as GeminiChatParamsNonStreaming);
     }
 }
