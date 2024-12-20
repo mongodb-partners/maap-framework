9259d6eb4c7e8771bc2ff6dd78bb4861f02c0ca1
SBarahona17 <steven.barahona@pureinsights.com>
2024-12-20 13:11:38 -0600

    deleting unused imports

--- src/convertMaapToChatbotFramework.ts
+++ src/convertMaapToChatbotFramework.ts
@@ -2,7 +2,7 @@
   @fileoverview Convert various constructs created by the MAAP dev team
   to work with the MongoDB Chatbot Framework.
  */
-import { Chunk, ExtractChunkData } from './global/types.js';
+import { ExtractChunkData } from './global/types.js';
 import { BaseEmbeddings } from './interfaces/base-embeddings.js';
 import { BaseLoader } from './interfaces/base-loader.js';
 import { BaseModel } from './interfaces/base-model.js';
