523498af3ec0e82da97aa01f29cc537b076bc7f0
SBarahona17 <steven.barahona@pureinsights.com>
2024-12-26 15:26:20 -0600

    Update fireworks-model.ts

--- src/models/langChain/fireworks-model.ts
+++ src/models/langChain/fireworks-model.ts
@@ -25,42 +25,20 @@ export class Fireworks extends BaseModel {
         this.model = new ChatFireworks({ temperature: this.temperature, maxTokens: this.maxTokens, model: this.modelName, apiKey: this.apiKey});
     }
 
-
-    private generatePastMessages(system: string, supportingContext: Chunk[], pastConversations: ConversationHistory[], userQuery: string) {
-        const pastMessages: (AIMessage | SystemMessage | HumanMessage)[] = [
-            new SystemMessage(
-                `${system}. Supporting context: ${supportingContext.map((s) => s.pageContent).join('; ')}`
-            ),
-        ];
-
-        pastMessages.push.apply(
-            pastMessages,
-            pastConversations.map((c) => {
-                if (c.sender === 'AI') return new AIMessage({ content: c.message });
-                else if (c.sender === 'SYSTEM') return new SystemMessage({ content: c.message });
-                else return new HumanMessage({ content: c.message });
-            })
-        );
-        pastMessages.push(new HumanMessage(`${userQuery}?`));
-
-        this.debug('Executing Fireworks model with prompt -', userQuery);
-        return pastMessages;
-    }
-
     override async runQuery(
         system: string,
         userQuery: string,
         supportingContext: Chunk[],
         pastConversations: ConversationHistory[],
     ): Promise<string> {
-        const pastMessages: (AIMessage | SystemMessage | HumanMessage)[] = this.generatePastMessages(system, supportingContext, pastConversations, userQuery);
+        const pastMessages: (AIMessage | SystemMessage | HumanMessage)[] = this.generatePastMessagesLangchain(system, supportingContext, pastConversations, userQuery);
         const result = await this.model.invoke(pastMessages);
         this.debug('Fireworks response -', result);
         return result.content.toString();
     }
 
     override async runStreamQuery(system: string, userQuery: string, supportingContext: Chunk[], pastConversations: ConversationHistory[]): Promise<any> {
-        const pastMessages: (AIMessage | SystemMessage | HumanMessage)[] = this.generatePastMessages(system, supportingContext, pastConversations, userQuery);
+        const pastMessages: (AIMessage | SystemMessage | HumanMessage)[] = this.generatePastMessagesLangchain(system, supportingContext, pastConversations, userQuery);
         const parser = new StringOutputParser();
         const stream = await this.model.pipe(parser).stream(pastMessages);
         return stream;
