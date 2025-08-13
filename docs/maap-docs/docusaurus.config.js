f25f7cf2b2078b42688c103b5ea42cfa432a1122
Utsav Talwar <utsav.talwar@mongodb.com>
2025-08-13 15:59:05 +0530

    Upadte docusorus

--- docs/maap-docs/docusaurus.config.js
+++ docs/maap-docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   themes: ['@docusaurus/theme-mermaid'],
 
   // Set the production url of your site here
-  url: 'https://mongodb-partners.github.io/maap-framework/',
+  url: 'https://mongodb-partners.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/maap-framework/',
